f66a44f32f5bd9c7122f418ac65be37a1f15c7d4
xhafan <xhafan@gmail.com>
2026-04-07 15:07:44 +0200

    #1 First version - moving RuntimeOptions and failed message error handlers code into CoreBackend

---
+++ CoreBackend/Infrastructure/Configurations/RuntimeOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace CoreBackend.Infrastructure.Configurations;
+
+public class RuntimeOptions
+{
+    public string Url { get; set; } = null!;
+    public string Environment { get; set; } = null!;
+    public string? AppGitShaVersion { get; set; }
+}
